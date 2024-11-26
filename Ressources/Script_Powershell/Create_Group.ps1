3007323a25f68ee11bd0b004da07d0c42239f2e0
CamilleCalvel <calvel.camille@gmail.com>
2024-11-26 16:11:34 +0100

    Update Create_Group.ps1

--- Ressources/Script_Powershell/Create_Group.ps1
+++ Ressources/Script_Powershell/Create_Group.ps1
@@ -1,10 +1,8 @@
 # Demander le nom du groupe à l'utilisateur
 $NomGroupe = Read-Host "Entrez le nom du groupe à créer"
 
-# Si le chemin OU n'est pas fourni en argument, demander à l'utilisateur
-if (-not $CheminOU) {
-    $CheminOU = Read-Host "Entrez le chemin de l'Unité Organisationnelle (OU) dans le format 'OU=Groupes,DC=mondomaine,DC=com'"
-}
+# Demander le chemin à l'utilisateur
+$CheminOU = Read-Host "Entrez le chemin de l'Unité Organisationnelle (OU) dans le format 'OU=Groupes,DC=mondomaine,DC=com'"
 
 # Demander la description du groupe
 $Description = Read-Host "Entrez une description pour le groupe"
